23613ba29b5d92bf46ec5b813bf857271c3a8a5e
Staritsin <staritsinar@gmail.com>
2025-04-09 01:22:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,5 +9,9 @@ def download():
         return jsonify({"error": "URL is missing"}), 400
     return jsonify({"message": f"Тут будет обработка ссылки: {url}"})
 
+import os
+
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
