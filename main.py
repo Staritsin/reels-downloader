024f7a94726ce7fc5cd819cbc9745553d404a649
Staritsin <staritsinar@gmail.com>
2025-04-09 02:36:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-from flask import Flask, request, jsonify
-
 app = Flask(__name__)
 
 @app.route("/download", methods=["GET"])
@@ -9,5 +7,8 @@ def download():
         return jsonify({"error": "URL is missing"}), 400
     return jsonify({"message": f"Получил ссылку: {url}"}), 200
 
+# ЭТО — ключевая строчка 👇
+# Это позволяет gunicorn видеть переменную `app`
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8000)
